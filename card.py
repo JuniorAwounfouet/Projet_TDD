a80d4d0f7f0d2ceb5646850419a7b72e906c5a64
JuniorAwounfouet <juniorawounfouet@gmail.com>
2026-02-12 15:19:51 +0100

    Add Card equality and hashing methods

--- card.py
+++ card.py
@@ -1,6 +1,15 @@
 class Card:
     RANKS = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
 
-    def init(self, rank_str, suit):
+    def __init__(self, rank_str, suit):
         self.rank = self.RANKS[rank_str]
         self.suit = suit
+
+    def __eq__(self, other):
+        """Deux cartes , même couleur"""
+        if not isinstance(other, Card):
+            return False
+        return self.rank == other.rank and self.suit == other.suit
+
+    def __hash__(self):
+        return hash((self.rank, self.suit))
--- test_poker.py
+++ test_poker.py
@@ -4,3 +4,11 @@ def test_card_creation():
     card = Card('A', 'H')
     assert card.rank == 14  # A = 14
     assert card.suit == 'H'
+
+def test_card_equality():
+    """Test que deux cartes identiques sont égales"""
+    card1 = Card('A', 'H')
+    card2 = Card('A', 'H')
+    card3 = Card('K', 'H')
+    assert card1 == card2
+    assert card1 != card3
