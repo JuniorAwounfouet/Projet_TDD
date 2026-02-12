c9feb359bb05b27a332d42ce87f54bf622223677
JuniorAwounfouet <juniorawounfouet@gmail.com>
2026-02-12 15:54:08 +0100

    update category rank

--- poker.py
+++ poker.py
@@ -1,20 +1,16 @@
 from collections import Counter
 from card import Card
 
-CATEGORY_RANKS = {'High Card': 0}
+CATEGORY_RANKS = {'High Card': 0, 'One Pair': 1}
 
 def get_high_card(cards):
-    """
-    Évalue une high card
-    Retourne: (catégorie, 5_cartes_choisies, clé_de_tri)
-    """
-    sorted_cards = sorted(cards, reverse=True)  # Trie décroissant
-    chosen = sorted_cards[:5]  # Les 5 meilleures
+    """ Évalue une high card """
+    sorted_cards = sorted(cards, reverse=True) 
+    chosen = sorted_cards[:5]
     ranks = tuple(c.rank for c in chosen)
     return 'High Card', chosen, (0,) + ranks
 
 def evaluate_hand(cards):
-    """Fonction principale d'évaluation"""
     if len(cards) != 7:
         raise ValueError("Une main doit avoir exactement 7 cartes")
     return get_high_card(cards)
